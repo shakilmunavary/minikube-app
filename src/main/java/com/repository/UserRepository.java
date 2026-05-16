a3b15d61677107248d7bf1bfae04b8a23e717190
shakilmunavary <shakil.ahamed@gmail.com>
2026-05-16 15:20:18 +0530

    Update UserRepository.java

--- src/main/java/com/repository/UserRepository.java
+++ src/main/java/com/repository/UserRepository.java
@@ -1,6 +1,6 @@
 package com.repository;
 
-import com.model.User;
+import com.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
